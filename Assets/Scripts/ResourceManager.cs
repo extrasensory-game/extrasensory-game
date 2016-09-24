d127effd41b4ec8a5f754994a660ccc252a51652
dijkstra33 <omgosu@rambler.ru>
2016-09-24 17:44:12 +0200

    Добавил класс Spirit

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -2,8 +2,30 @@
 
 namespace ExtrasensoryGame
 {
+    using Data;
+
     public class ResourceManager : MonoBehaviour
     {
+        private ClientGenerator _clientGenerator;
 
+        private SpiritData[] _spirits;
+
+        private void Start()
+        {
+            this.LoadSpirits();
+        }
+
+        // TODO add API sufficient for any game mode script.
+
+        public Client GetNextClient()
+        {
+            return _clientGenerator.GetClient();
+        }
+
+        private void LoadSpirits()
+        {
+            // TODO REALIZE THIS.
+            this._spirits = new SpiritData[0];
+        }
     }
 }
