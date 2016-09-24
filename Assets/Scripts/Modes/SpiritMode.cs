3740772860fe22a4709ce2dde6e3e7fa3f693c2a
SemikX <semikx@mail.ru>
2016-09-24 20:27:43 +0200

    Кнопки пофиксили, глаз вроде работает

--- Assets/Scripts/Modes/SpiritMode.cs
+++ Assets/Scripts/Modes/SpiritMode.cs
@@ -1,6 +1,7 @@
 ﻿using ExtrasensoryGame;
 using ExtrasensoryGame.Data;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpiritMode : IMode
 {
@@ -10,20 +11,41 @@ public class SpiritMode : IMode
     private Client client;
     private SpiritData spiritData;
     private GameObject clientObject;
+    private readonly GameObject clientPanel;
+
+    public SpiritMode(Client client, SpiritData spiritData, GameObject clientPanel)
+    {
+        this.client = client;
+        this.spiritData = spiritData;
+        this.clientPanel = clientPanel;
+    }
 
     public void Init()
     {
-        _spiritObject = GameObject.Instantiate(this.spiritData.Prefab);
         this.clientObject = new GameObject();
 
         foreach (var sprite in client.CharacterSprites)
         {
             InstantiateSprite(sprite);
         }
+        this.clientPanel.GetComponentInChildren<Button>().onClick.AddListener(ShowSpirit);
     }
 
     public void Update()
     {
+    }
+
+    public bool IsFinished()
+    {
+        // ++_framesCount;
+        return _framesCount > 200;
+    }
+
+    public void Deinit()
+    {
+        this.clientPanel.GetComponentInChildren<Button>().onClick.RemoveListener(ShowSpirit);
+        if (_spiritObject != null)
+            GameObject.Destroy(_spiritObject);
     }
 
     private void InstantiateSprite(SpriteInstance spriteInstance)
@@ -32,19 +54,8 @@ public class SpiritMode : IMode
         go.transform.parent = this.clientObject.transform;
     }
 
-    public SpiritMode(Client client, SpiritData spiritData)
+    private void ShowSpirit()
     {
-        this.client = client;
-        this.spiritData = spiritData;
-    }
-
-    public bool IsFinished()
-    {
-        ++_framesCount;
-        return _framesCount > 200;
-    }
-
-    public void Deinit()
-    {
+        _spiritObject = GameObject.Instantiate(this.spiritData.Prefab);
     }
 }
