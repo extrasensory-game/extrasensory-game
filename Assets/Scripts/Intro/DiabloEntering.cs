5509a0451a4a6727687f1504be39e9421253babd
EvgeniyAA <andreeff.ea@yandex.ru>
2016-09-24 20:38:55 +0200

    вступительная сцена beta

--- Assets/Scripts/Intro/DiabloEntering.cs
+++ Assets/Scripts/Intro/DiabloEntering.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using ExtrasensoryGame.Assets.Scripts.Intro;
+using UnityEngine.SceneManagement;
 
 public class DiabloEntering : MonoBehaviour
 {
@@ -9,6 +10,22 @@ public class DiabloEntering : MonoBehaviour
     
     public void OnAnimationEnd()
     {
+        StartCoroutine(WaitAndOpenPanel()); 
+    }
+
+    private IEnumerator WaitAndOpenPanel()
+    {
+        yield return new WaitForSeconds(1);
         scrollPanel.OpenPanel();
+    }
+
+    public void Leave()
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("Leave");
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Main");
     }
 }
