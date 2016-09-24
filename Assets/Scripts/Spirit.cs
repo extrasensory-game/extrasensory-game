cc3d75e33f651cff1b94b5c8bee263410b319d23
dijkstra33 <omgosu@rambler.ru>
2016-09-24 20:50:53 +0200

    поправил метод ApplyItem и добавил методы для вызова диалога в Spirit
    поправил метод ApplyItem и добавил методы для вызова диалога в Spirit
    а ещё подпилил логику убирающую использованные Phrases из SpiritData

--- Assets/Scripts/Spirit.cs
+++ Assets/Scripts/Spirit.cs
@@ -2,37 +2,40 @@
 
 namespace ExtrasensoryGame.Data
 {
+    using SpiritDialogs;
+
     public class Spirit : MonoBehaviour
     {
         private SpiritData _spiritData;
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         public void Initialize(SpiritData spiritData)
         {
             this._spiritData = spiritData;
         }
 
-        public void ApplyItem(int itemId)
+        public void ApplyItem(ItemData itemData)
         {
-            // TODO get real item rage value.
-            var itemRageValue = 10;
+            var rageValue = this._spiritData.ApplyItem(itemData);
 
-            var rageValue = this._spiritData.ApplyItem(itemId, itemRageValue);
-
-            // TODO Set rage on slider.
+            this.UpdateRageBar();
             if (StateChanged())
                 ApplyStateChanges();
+        }
+
+        public SpiritPhrase[] GetSpiritPhrases()
+        {
+            return this._spiritData.HasNextDialog() ? this._spiritData.GetSpiritPhrases() : new SpiritPhrase[0];
+        }
+
+        public void SelectPhrase(SpiritPhrase phrase)
+        {
+            this._spiritData.SelectPhrase(phrase);
+            this.UpdateRageBar();
+        }
+
+        private void UpdateRageBar()
+        {
+            // TODO.
         }
 
         private SpiritState prevSpiritState = SpiritState.Neutral;
