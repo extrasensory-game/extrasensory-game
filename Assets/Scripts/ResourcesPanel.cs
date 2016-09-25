f0dc248560a804ff27b3d1c435a6fe7adb3ad7a7
SealTV <seal.matob@gmail.com>
2016-09-25 16:34:19 +0200

    Добавил связь с очками

---
+++ Assets/Scripts/ResourcesPanel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ExtrasensoryGame
+{
+    public class ResourcesPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _money;
+        [SerializeField]
+        private Text _spirits;
+        [SerializeField]
+        private Text _hum;
+        [SerializeField]
+        private Text _quack;
+
+        private void Update()
+        {
+            _money.text = string.Format("{0}", Game.Instance.Player.Money);
+            _spirits.text = string.Format("{0}", Game.Instance.Player.SpiritPoints);
+            _hum.text = string.Format("{0}", Game.Instance.Player.HumanityPoints);
+            _quack.text = string.Format("{0}", Game.Instance.Player.QuackPoints);
+        }
+    }
+}
